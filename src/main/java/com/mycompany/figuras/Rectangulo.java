b9082d2727467f35af5f17cd183468ec93f3795f
Samuel Ortiz <sortiz@domain-system.local>
2020-05-30 16:26:50 +0200

    Creacion del metodo 'ObtenerPerimetro'

---
+++ src/main/java/com/mycompany/figuras/Rectangulo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.figuras;
+
+/**
+ *
+ * @author sortiz
+ */
+public class Rectangulo {
+    int l1,l2;
+    int perimetro;
+    public void perimetro(){
+        perimetro = 2*l1 + 2*l2;
+        
+    }
+}
